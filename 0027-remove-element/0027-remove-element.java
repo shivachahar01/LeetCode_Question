97c3f84a1ff4f3af4f17641c8e31bee8a290e3f3
Shiva chahar <shivachahar0001@gmail.com>
2026-08-13 19:08:28 +0530

    Time: 0 ms (100%), Space: 43.6 MB (43.65%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,9 +3,8 @@ class Solution {
         int j = 0 ;
         for(int i=0 ; i<nums.length ; i++){
             if(nums[i] != val){
-                int temp = nums[i];
-                nums[i]=nums[j];
-                nums[j]= temp;
+                nums[j]=nums[i];
+                
                 j++;
             }
         }
