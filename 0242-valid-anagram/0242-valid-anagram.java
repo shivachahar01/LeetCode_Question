813959298363e3579ff368a82daf416d07d4c258
Shiva chahar <shivachahar0001@gmail.com>
2026-08-17 13:21:39 +0530

    Time: 4 ms (83.02%), Space: 46.6 MB (25.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,11 +4,6 @@ class Solution {
         char[] a2 = t.toCharArray();
         Arrays.sort(a1);
         Arrays.sort(a2);
-        if(Arrays.equals(a1,a2)){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return Arrays.equals(a1,a2);
     }
 }
