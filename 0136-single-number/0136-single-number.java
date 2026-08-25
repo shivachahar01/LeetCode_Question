90ae77e12095f71988acf5e47f3a58d9b7fad7a2
Shiva chahar <shivachahar0001@gmail.com>
2026-08-25 22:09:28 +0530

    Time: 8 ms (27.91%), Space: 47.1 MB (13.78%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0 ;
-       for(int i = 0 ; i <nums.length ; i++){
-        result = result^nums[i];
-       }
-       return result;
+        int freq[] = new int[1000001];
+        for(int i = 0 ; i <nums.length ; i++){
+            freq[nums[i]+3000]++;
+        }
+        for(int i = 0 ; i <freq.length;i++){
+        if(freq[i]==1){
+            return i-3000;
+        }}
+        return -1;
     }
 }
