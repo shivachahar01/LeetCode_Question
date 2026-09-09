cf068fad6e22b93ee86222a1102cf11126e9e239
Shiva chahar <shivachahar0001@gmail.com>
2026-09-09 22:32:07 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        if(n<1000){
+            return 0;
+        }
+
+        if (n >=1000) {
+            ans += (n - 1000) + 1;
+        }
+
+        if (n >= 1000000) {
+            ans += (n - 1000000 )+ 1;
+        }
+
+        if (n >= 1000000000L) {
+            ans += (n - 1000000000L )+ 1;
+        }
+
+        if (n >= 1000000000000L) {
+            ans += (n - 1000000000000L )+ 1;
+        }
+        if (n >= 1000000000000000L) {
+            ans += (n - 1000000000000000L )+ 1;
+        }
+        
+
+        return ans;
+    }
+}
