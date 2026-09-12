eaa8a61e9185472e7f5271078a680f166fe94d3c
Shiva chahar <shivachahar0001@gmail.com>
2026-09-13 00:30:45 +0530

    Time: 8 ms (16.39%), Space: 47.9 MB (10.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,33 +1,33 @@
-// class Solution {
-//     public int missingNumber(int[] nums) {
-//         Arrays.sort(nums);
-//         if(nums[0]==1){
-//             return 0;
-//         }
-//         for(int i = 0 ; i <nums.length-1 ; i++){
-//               if(nums[i+1]-nums[i]!=1){
-//                 return nums[i]+1;
-//               }
-//         }
-//         return nums.length;
-//     }
-// }
-
 class Solution {
-   public int missingNumber(int[] arr) {
-
-        long sum1 = 0;
-        long sum2 = 0;
-        Arrays.sort(arr);
-       int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            sum1 += arr[i];
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        if(nums[0]==1){
+            return 0;
         }
-
-        for (int i = 0; i <n+1; i++) {
-            sum2 += i;
+        for(int i = 0 ; i <nums.length-1 ; i++){
+              if(nums[i+1]-nums[i]!=1){
+                return nums[i]+1;
+              }
         }
-
-        return (int)(sum2 - sum1);
+        return nums.length;
     }
 }
+
+// class Solution {
+//    public int missingNumber(int[] arr) {
+
+//         long sum1 = 0;
+//         long sum2 = 0;
+//         Arrays.sort(arr);
+//        int n = arr.length;
+//         for (int i = 0; i < n; i++) {
+//             sum1 += arr[i];
+//         }
+
+//         for (int i = 0; i <n+1; i++) {
+//             sum2 += i;
+//         }
+
+//         return (int)(sum2 - sum1);
+//     }
+// }
