5ff367822c4f341468b7051c440cc8c782aa3fcf
Shiva chahar <shivachahar0001@gmail.com>
2026-09-14 10:49:01 +0530

    Time: 7 ms (45.07%), Space: 63.3 MB (6.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
-        
+       Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
